3d18619036be2a0fa79a360a626b753007939547
qzink <zhaoyimin927@outlook.com>
2025-06-13 17:10:24 +0800

    添加ViewModelBase类

--- ViewModels/HomePageViewModel.cs
+++ ViewModels/HomePageViewModel.cs
@@ -18,7 +18,7 @@ using Windows.Storage.Provider;
 
 namespace SpotlightGallery.ViewModels
 {
-    public class HomePageViewModel : INotifyPropertyChanged
+    public class HomePageViewModel : ViewModelBase
     {
         private readonly IWallpaperService wallpaperService;
         private Wallpaper wallpaper;
@@ -278,23 +278,6 @@ namespace SpotlightGallery.ViewModels
             {
                 infoBarTimer.Stop();
             }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(storage, value))
-                return false;
-
-            storage = value;
-            OnPropertyChanged(propertyName);
-            return true;
         }
     }
 }
