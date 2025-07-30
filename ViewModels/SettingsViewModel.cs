d1121cd7834c95a797a9a769303eb4d8dcc838de
qzink <zhaoyimin927@outlook.com>
2025-07-30 16:10:10 +0800

    feat: add wallpaper source and resolution selection

--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Storage;
 using SpotlightGallery.Helpers;
-using Microsoft.UI.Xaml;
+using SpotlightGallery.Services;
 
 namespace SpotlightGallery.ViewModels
 {
     public class SettingsViewModel : ViewModelBase
     {
+        private readonly IWallpaperService wallpaperService = ServiceLocator.WallpaperService;
+
         private int appThemeIndex;
         /// <summary>
         /// 0 - 浅色, 1 - 深色, 2 - 系统默认
@@ -28,7 +30,60 @@ namespace SpotlightGallery.ViewModels
             }
         }
 
+        public ObservableCollection<string> ResolutionOptions { get; } = new();
 
+        private int sourceIndex;
+
+        /// <summary>
+        /// 0 - Spotlight, 1 - Bing
+        /// </summary>
+        public int SourceIndex
+        {
+            get => sourceIndex;
+            set
+            {
+                if (SetProperty(ref sourceIndex, value))
+                {
+                    SettingsHelper.SaveSetting("Source", value);
+                    UpdateResolutionOptions();
+                    ResolutionIndex = 0;
+                    wallpaperService.ChangeSource((WallpaperSource)value, 0);
+                }
+            }
+        }
+
+        private int resolutionIndex;
+
+        /// <summary>
+        /// Spotlight: 0 - 3840x2160(Spotlight Desktop), 1 - 1920x1080(Spotlight Lockscreen)
+        /// </summary>
+        public int ResolutionIndex
+        {
+            get => resolutionIndex;
+            set
+            {
+                if (SetProperty(ref resolutionIndex, value))
+                {
+                    SettingsHelper.SaveSetting("Resolution", value);
+                    wallpaperService.ChangeSource((WallpaperSource)SourceIndex, value);
+                }
+            }
+        }
+
+        private void UpdateResolutionOptions()
+        {
+            ResolutionOptions.Clear();
+            if ((WallpaperSource)SourceIndex == WallpaperSource.Spotlight)
+            {
+                ResolutionOptions.Add("3840x2160");
+                ResolutionOptions.Add("1920x1080");
+            }
+            else if ((WallpaperSource)SourceIndex == WallpaperSource.BingDaily)
+            {
+                ResolutionOptions.Add("1920x1080");
+                // 可扩展更多分辨率
+            }
+        }
 
         public SettingsViewModel()
         {
@@ -38,6 +93,9 @@ namespace SpotlightGallery.ViewModels
         private void LoadSettings()
         {
             AppThemeIndex = SettingsHelper.GetSetting("AppTheme", 2);
+            SourceIndex = SettingsHelper.GetSetting("Source", 0);
+            ResolutionIndex = SettingsHelper.GetSetting("Resolution", 0);
+            UpdateResolutionOptions();
         }
     }
 }
