5bb39fd28ea731f5c6442d08e8e2f04b8e72939f
qzink <zhaoyimin927@outlook.com>
2025-08-01 12:38:50 +0800

    feat: implement wallpaper update background task

--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using SpotlightGallery.Helpers;
 using SpotlightGallery.Services;
+using Windows.ApplicationModel.Background;
 
 namespace SpotlightGallery.ViewModels
 {
@@ -92,6 +93,130 @@ namespace SpotlightGallery.ViewModels
             }
         }
 
+        private bool isAutoUpdateEnabled;
+        public bool IsAutoUpdateEnabled
+        {
+            get => isAutoUpdateEnabled;
+            set
+            {
+                if (SetProperty(ref isAutoUpdateEnabled, value) && isInitialized)
+                {
+                    SettingsHelper.SaveSetting("AutoUpdate", value);
+                    SettingsHelper.SaveSetting("NextUpdateTime", DateTimeOffset.Now.AddDays(1));
+                    if (value)
+                    {
+                        _ = RegisterWallpaperUpdateTaskAsync();
+                    }
+                    else
+                    {
+                        UnregisterWallpaperUpdateTask();
+                    }
+                }
+            }
+        }
+
+        private int updateModeIndex;
+        /// <summary>
+        /// 0 - daily, 1 - at specific time
+        /// </summary>
+        public int UpdateModeIndex
+        {
+            get => updateModeIndex;
+            set
+            {
+                if (SetProperty(ref updateModeIndex, value) && isInitialized)
+                {
+                    SettingsHelper.SaveSetting("UpdateMode", value);
+                    if (value == 0) // daily
+                    {
+                        SettingsHelper.SaveSetting("NextUpdateTime", new DateTimeOffset(DateTimeOffset.Now.Date.AddDays(1)));
+                    }
+                    else // at specific time
+                    {
+                        SettingsHelper.SaveSetting("NextUpdateTime", new DateTimeOffset(DateTimeOffset.Now.Date.AddDays(1)).Add(UpdateTime));
+                    }
+
+                    OnPropertyChanged(nameof(IsUpdateTimeEnabled));
+                }
+            }
+        }
+
+        public bool IsUpdateTimeEnabled => UpdateModeIndex == 1;
+
+        private TimeSpan updateTime;
+        /// <summary>
+        /// time when the wallpaper should be updated if UpdateModeIndex is set to 1 (at specific time)
+        /// </summary>
+        public TimeSpan UpdateTime
+        {
+            get => updateTime;
+            set
+            {
+                if (SetProperty(ref updateTime, value) && isInitialized)
+                {
+                    SettingsHelper.SaveSetting("UpdateTime", value);
+                    DateTimeOffset nextUpdateTime = DateTimeOffset.Now.Date.Add(updateTime);
+                    if (nextUpdateTime <= DateTimeOffset.Now)
+                    {
+                        nextUpdateTime = nextUpdateTime.AddDays(1);
+                    }
+                    SettingsHelper.SaveSetting("NextUpdateTime", nextUpdateTime);
+                }
+            }
+        }
+
+        public async Task RegisterWallpaperUpdateTaskAsync()
+        {
+            try
+            {
+                foreach (var task in BackgroundTaskRegistration.AllTasks)
+                {
+                    if (task.Value.Name == "WallpaperUpdateTask")
+                    {
+                        task.Value.Unregister(true);
+                    }
+                }
+
+                var status = await BackgroundExecutionManager.RequestAccessAsync();
+                if (status == BackgroundAccessStatus.DeniedByUser || status == BackgroundAccessStatus.DeniedBySystemPolicy)
+                    return;
+
+                var builder = new BackgroundTaskBuilder
+                {
+                    Name = "WallpaperUpdateTask",
+                };
+                builder.SetTaskEntryPointClsid(typeof(BackgroundTasks.WallpaperUpdateTask).GUID);
+
+                // run background task every 4 hours to check next update time
+                builder.SetTrigger(new TimeTrigger(240, false));
+
+                builder.Register();
+
+                bool isRegistered = BackgroundTaskRegistration.AllTasks
+                    .Any(t => t.Value.Name == "WallpaperUpdateTask");
+                System.Diagnostics.Debug.WriteLine(isRegistered
+                    ? "WallpaperUpdateTask registered successfully."
+                    : "Failed to register WallpaperUpdateTask.");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"RegisterWallpaperUpdateTaskAsync Exception: {ex}");
+            }
+        }
+
+        private void UnregisterWallpaperUpdateTask()
+        {
+            // 取消注册任务
+            foreach (var task in BackgroundTaskRegistration.AllTasks)
+            {
+                if (task.Value.Name == "WallpaperUpdateTask")
+                {
+                    task.Value.Unregister(true);
+                    break;
+                }
+            }
+        }
+
         public SettingsViewModel()
         {
             LoadSettings();
@@ -106,6 +231,10 @@ namespace SpotlightGallery.ViewModels
             ResolutionIndex = SettingsHelper.GetSetting("Resolution", 0);
             UpdateResolutionOptions();
             wallpaperService.ChangeSource((WallpaperSource)SourceIndex, ResolutionIndex);
+            // load auto update settings
+            IsAutoUpdateEnabled = SettingsHelper.GetSetting("AutoUpdate", false);
+            UpdateModeIndex = SettingsHelper.GetSetting("UpdateMode", 0);
+            UpdateTime = SettingsHelper.GetSetting("UpdateTime", TimeSpan.FromHours(12));
         }
     }
 }
