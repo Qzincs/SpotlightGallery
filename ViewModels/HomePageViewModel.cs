2585d1caef0e6f02ef321c02529477731181bf4a
Qzincs <zhaoyimin927@outlook.com>
2025-12-14 21:26:03 +0800

    feat: add informational message for Bing Daily Wallpaper limit

--- ViewModels/HomePageViewModel.cs
+++ ViewModels/HomePageViewModel.cs
@@ -132,6 +132,13 @@ namespace SpotlightGallery.ViewModels
             {
                 if (IsLoading) return;
 
+                if (wallpaperService.CurrentSource == WallpaperSource.BingDaily && 
+                    Wallpaper?.url?.Contains("bing") == true)
+                {
+                    ShowInfoBar(ResourceLoader.GetString("InfoBar_BingDailyLimit"), InfoBarSeverity.Informational);
+                    return;
+                }
+
                 try
                 {
                     IsLoading = true;
